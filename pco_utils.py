47619ad676d307e190b5aa4544dff829325107ee
Christopher Ellis <christopherellis@Christophers-iMac.local>
2021-02-15 16:01:56 -0800

    Create pipeline for song data

--- pco_utils.py
+++ pco_utils.py
@@ -2,10 +2,12 @@ from datetime import datetime, timedelta
 import fetch as f
 
 today = datetime.now()
+service_ids = []
 
 
 def pco_people_pipeline():
     service_type_ids = f.get_pco_service_types()
+    service_ids.extend(service_type_ids)
     plan_ids = [f.get_pco_plans(service_id) for service_id in service_type_ids]
     upcoming_plans = []
     for item in plan_ids:
@@ -16,6 +18,15 @@ def pco_people_pipeline():
                 if people:
                     upcoming_plans.append(people)
     return upcoming_plans
+
+
+def pco_songs_pipeline():
+    songs_used = []
+    for service_id in service_ids:
+        plan_ids = f.get_past_pco_plans(service_id)
+        for plan in plan_ids:
+            songs_used.extend(f.get_plan_items(service_id, plan))
+    return songs_used
 
 
 def status_stats(pco_people):
@@ -42,8 +53,8 @@ def is_it_this_week(plan_sort_date):
         return True
 
 
-
 pco_people = pco_people_pipeline()
 two_weeks_plans = date_sort_pipeline(pco_people)
 people_stats = status_stats(two_weeks_plans)
-two_weeks_sorted = sorted(two_weeks_plans, key= lambda plan:plan['sort_date'])
+two_weeks_sorted = sorted(two_weeks_plans, key=lambda plan: plan['sort_date'])
+songs = pco_songs_pipeline()
